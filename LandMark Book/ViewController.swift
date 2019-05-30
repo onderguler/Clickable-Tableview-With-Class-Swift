be3b930e94a03f655b05c091f021f0be3543792c
ÖnderGüler <onder031@gmail.com>
2019-05-30 16:13:05 +0300

    Initial Commit

---
+++ LandMark Book/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LandMark Book
+//
+//  Created by önder güler on 30.05.2019.
+//  Copyright © 2019 önder güler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
